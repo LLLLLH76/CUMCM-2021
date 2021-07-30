4b640894f21e2783554c9a8b8f002d92a4e4a8d2
LLLLH76 <56062479+LLLLLH76@users.noreply.github.com>
2021-07-30 08:39:51 +0800

    Update MC.py

--- MC.py
+++ MC.py
@@ -9,7 +9,7 @@ def translate2tangent(e, l):
     '''
     o = Point(0,0)                      # 原点
     l_vertical = Line(o, slope = -1/l.slope)    # l 的过原点的垂线
-    p0 = e.intersection(l_vertical)[0] # 垂线与椭圆的一个交点
+    p0 = e.intersection(l_vertical)[0]          # 垂线与椭圆的一个交点
     lower_bound = p0.distance(o).evalf()        # 切点必然在垂线交点"外部", 因此以交点与原点距离为下界
     upper_bound = np.inf
     l_vertical = Segment(3*p0, -3*p0)
@@ -20,8 +20,8 @@ def translate2tangent(e, l):
             print(p.distance(o).evalf())
             continue
         l_parallel = Line(p, slope = l.slope)
-        intersection_list = e.intersection(l_parallel)
-        if len(intersection_list) == 1: # 刚好相切, 不可能
+        intersection_list = e.intersection(l_parallel) # 会卡死
+        if len(intersection_list) == 1:   # 刚好相切, 不可能
             return l_parallel
         elif len(intersection_list) == 0: # 相离, 缩小线段
             upper_bound = p.distance(o)
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     B = random.randint(1,10)    # 短轴
     e = Ellipse(o, A, B)        # 创建椭圆
     print(e.area.evalf())       # 椭圆面积
-    p1 = e.random_point()       # 随机找一个点p1
+    p1 = e.random_point()       # 随机找一个椭圆上的点p1
     l1 = e.tangent_lines(p1)[0] # 椭圆在p1的切线l1
     a = 2 * l1.distance(o)      # 六边形一组对边距离, 即a
     l2_parallel = l1.rotate( 2 * np.pi / 3) # l1旋转 120度得到六边形邻边l2的平行边
